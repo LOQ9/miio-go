93f58dae7d5fabdfbafa94509b75e0f3fc74a241
Marco Santos <marco.santos@glartek.com>
2023-01-03 20:10:53 +0000

    chore: Remove unecessary files

--- bin/install_tools.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-go get github.com/mattn/goveralls
-go get github.com/haya14busa/goverage
-go get github.com/vektra/mockery/.../
--- bin/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-ROOT_DIR=$(cd "$(dirname "$0")"/.. && pwd)
-cd ${ROOT_DIR}
-
-go test -v --timeout 1s ./...
